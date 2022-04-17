9903d61b34a843929fec72bf3bda07d51b73e7b8
leravel888 <63417104+leravel888@users.noreply.github.com>
2022-04-17 22:44:10 +0400

    Task2

---
+++ ConsoleApp1/Task2.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ConsoleApp1
+{
+    class Task2
+    {
+        int speed;
+        float weight;
+        bool isMove;
+        string namePlayer;
+        ushort hitCount;
+        byte healthPoint;
+        decimal damagePercent;
+        double distanceToSavePoint;
+        uint averagePlayerSpeed;
+        ulong countOfCoinsCollected;
+    }
+}
