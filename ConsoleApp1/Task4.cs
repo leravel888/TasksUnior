b820a665a32c5551b103bb0b3d707dc1705d660c
leravel888 <63417104+leravel888@users.noreply.github.com>
2022-04-18 01:22:49 +0400

    Task4

---
+++ ConsoleApp1/Task4.cs
@@ -0,0 +1,68 @@
+﻿using System;
+ 
+namespace ConsoleApp1
+{
+    class Task4
+    {       
+        public string name;
+        string zodiacSign;
+        int age;
+        string favoriteMovie;
+
+        static void Main(string[] args)
+        {
+            Task4 user = new Task4() ;
+            
+            Console.Write("Как вас зовут - ");
+            user.name = Console.ReadLine();
+
+            Console.Write("Какой ваш знак зодиака - ");
+            user.zodiacSign = Console.ReadLine();
+
+            Console.Write("Сколько вам лет - ");
+            user.age = user.ChekEnterAge(Console.ReadLine());
+
+            Console.Write("Какой ваш любимый фильм - ");
+            user.favoriteMovie = Console.ReadLine();
+            
+            Console.WriteLine("Вас зовут {0}, по знаку зодиака вы {1}, вам {2} {3}, ваш любимый фильм \"{4}\".", 
+            user.name, user.zodiacSign, user.age, user.CheckDeclinationAge(user.age), user.favoriteMovie);
+        
+
+            Console.ReadKey();
+
+        }
+
+        public int ChekEnterAge(string ageString)
+        {
+            int ageInt = 0; 
+            while (!int.TryParse(ageString, out ageInt) || !(ageInt > 3 && ageInt < 100))
+            {
+                Console.Write("Введите корректный возраст - ");
+                ageString = Console.ReadLine();
+            }
+            return ageInt;
+        }
+
+        public string CheckDeclinationAge(int ageInt)
+        {
+            string declinationAge = "";
+
+            if (ageInt % 10 == 1 && (ageInt % 100)/10 != 1)
+            {
+                declinationAge = "год";
+            }
+            else if ((ageInt % 10 >= 2 && ageInt % 10 <= 4) && (ageInt % 100) / 10 != 1)
+            {
+                declinationAge = "года";
+            }
+            else 
+            {
+                declinationAge = "лет";
+            }
+
+            return declinationAge;
+        }
+
+    }
+}
