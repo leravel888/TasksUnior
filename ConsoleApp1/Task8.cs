47610ab221d1584a95a76c4182e4d95a78c32310
leravel888 <63417104+leravel888@users.noreply.github.com>
2022-04-19 01:16:55 +0400

    Create Task8.cs

---
+++ ConsoleApp1/Task8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Task8
+    {
+        static void Main()
+        {
+            int countPeople;
+            int minutesInHour = 60;
+            int timeOfReception = 10;
+            string waitTime;
+
+            Console.Write("Кол-во людей в очереди - ");
+            countPeople =Int32.Parse(Console.ReadLine());
+
+            waitTime = "Осталось ждать "+(countPeople * timeOfReception) / minutesInHour + " час(а) " 
+            + (countPeople * timeOfReception) % minutesInHour + " минут.";
+            Console.WriteLine(waitTime);
+
+            Console.ReadLine();
+        }
+    }
+}
