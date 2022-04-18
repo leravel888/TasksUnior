683fcd3898918fc4e009f48a05e508fb202d7c99
leravel888 <63417104+leravel888@users.noreply.github.com>
2022-04-19 00:57:04 +0400

    Create Task7.cs

---
+++ ConsoleApp1/Task7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Task7
+    {
+        static void Main()
+        {
+            int countCoin;
+            int countCrystal = 0;
+            int priceCrystal = 13;
+            int countCrystalsToBuy;
+
+            Console.Write("Введите кол-во золота - ");
+            countCoin =Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Цена одного кристалла - " + priceCrystal);
+            Console.Write("Сколько кристаллов вы хотите купить - ");
+
+            countCrystalsToBuy = Int32.Parse(Console.ReadLine());
+
+            countCrystal += countCrystalsToBuy;
+            countCoin -= countCrystalsToBuy * priceCrystal;
+
+            Console.WriteLine("Осталось золота - {0}, кристаллов - {1} ",countCoin, countCrystal);
+
+            Console.ReadLine();
+
+        }
+    }
+}
