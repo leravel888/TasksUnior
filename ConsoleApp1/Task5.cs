660c39e11f80cf9e189098c16149992cbb16c1d2
leravel888 <63417104+leravel888@users.noreply.github.com>
2022-04-18 23:50:09 +0400

    Task5

---
+++ ConsoleApp1/Task5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+ 
+namespace ConsoleApp1
+{
+    class Task5
+    {             
+        static void Main()
+        {           
+            string firstName = "aaa";
+            string lastName = "bbb";
+            string tmpReplacement = firstName;
+
+            Console.WriteLine("{0} {1}" , firstName, lastName);
+
+            firstName = lastName;
+            lastName = tmpReplacement;
+
+            Console.WriteLine("{0} {1}", firstName, lastName);
+
+            Console.ReadLine();
+        }
+    }
+}
