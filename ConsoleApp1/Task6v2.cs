4205bdcefee07076ae2c1180b49ea0fff08ceabf
leravel888 <63417104+leravel888@users.noreply.github.com>
2022-04-19 00:32:06 +0400

    Create Task6v2.cs

---
+++ ConsoleApp1/Task6v2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Task6v2
+    {
+        static void Main()
+        {
+            int countOfImages = 52;
+            int countOfImagesInRow = 3;
+
+            int filledRows = countOfImages / countOfImagesInRow;
+            int excessImages = countOfImages - (filledRows * countOfImagesInRow);
+
+            Console.WriteLine("Кол-во заполненных рядов - " + filledRows);
+            Console.WriteLine("Кол-во лишних картинок - " + excessImages);
+
+            Console.ReadLine();
+        }
+    }
+}
