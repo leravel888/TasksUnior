050ce854172c4d887a7c7575990e74bedd652a37
leravel888 <63417104+leravel888@users.noreply.github.com>
2022-04-18 22:21:05 +0400

    Task4v2

---
+++ ConsoleApp1/Task4v2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Task4v2
+    {
+
+        static void Main(string[] args)
+        {
+            string name;
+            string zodiacSign;
+            string age;
+            string favoriteMovie;
+
+            Console.Write("Как вас зовут - ");
+            name = Console.ReadLine();
+
+            Console.Write("Какой ваш знак зодиака - ");
+            zodiacSign = Console.ReadLine();
+
+            Console.Write("Сколько вам лет - ");
+            age = Console.ReadLine();
+
+            Console.Write("Какой ваш любимый фильм - ");
+            favoriteMovie = Console.ReadLine();
+
+            Console.WriteLine("Вас зовут {0}, по знаку зодиака вы {1}, вам {2}, ваш любимый фильм \"{3}\".",
+            name, zodiacSign, age, favoriteMovie);
+
+
+            Console.ReadKey();
+
+        }
+
+
+
+
+    }
+}
